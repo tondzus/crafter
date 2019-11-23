43bbeeb4ff32f017c5e1d8f8b9db5f2bbc40d621
Tomas Stibrany <tms.stibrany@gmail.com>
2019-11-24 00:20:42 +0100

    making item processing method private

--- crafter/pipeline.py
+++ crafter/pipeline.py
@@ -25,13 +25,13 @@ class Pipeline:
 
     def process(self, iterable):
         for item in iterable:
-            yield from self.depth_first_item_process(0, item)
+            yield from self._depth_first_item_process(0, item)
 
-    def depth_first_item_process(self, stage_number, item):
+    def _depth_first_item_process(self, stage_number, item):
         if len(self.stages) <= stage_number:
             yield item
             return
 
         step_callable = self.stages[stage_number]
         for processed_item in step_callable(item):
-            yield from self.depth_first_item_process(stage_number + 1, processed_item)
+            yield from self._depth_first_item_process(stage_number + 1, processed_item)
